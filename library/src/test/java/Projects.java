5bb564c47e24068243fa2cdd3c78164f0461e4ee
YeDaxia <yeguozhong@yeah.net>
2017-08-10 18:31:21 +0800

    change project info

--- library/src/test/java/Projects.java
+++ library/src/test/java/Projects.java
@@ -2,10 +2,10 @@
  * @author yeguozhong yedaxia.github.com
  */
 public class Projects {
-    public static String SpringProject = "/Users/yeguozhong/IdeaProjects/play-apidocs/springDemo/";
-    public static String PlayProject = "/Users/yeguozhong/IdeaProjects/play-apidocs/playDemo/";
-    public static String GenericProject = "/Users/yeguozhong/IdeaProjects/play-apidocs/genericDemo/";
-    public static String JFinalProject = "/Users/yeguozhong/IdeaProjects/play-apidocs/jfinalDemo";
+    public static String SpringProject = "/Users/yeguozhong/Desktop/gitLibrary/JApiDocs/springDemo/";
+    public static String PlayProject = "/Users/yeguozhong/Desktop/gitLibrary/JApiDocs/playDemo/";
+    public static String GenericProject = "/Users/yeguozhong/Desktop/gitLibrary/JApiDocs/genericDemo/";
+    public static String JFinalProject = "/Users/yeguozhong/Desktop/gitLibrary/JApiDocs/jfinalDemo";
 
-    public static String DocsPath = "/Users/yeguozhong/IdeaProjects/play-apidocs/docs";
+    public static String DocsPath = "/Users/yeguozhong/Desktop/gitLibrary/JApiDocs/JApiDocs/docs";
 }
